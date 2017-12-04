efb378a66a86029b6a9dea4922be31c7649929e3
retractive <cai.willa@gmail.com>
2017-12-04 14:42:58 -0600

    declared implementation of website mining portion, outlined training and prediction of characters

--- src/FireEmblem.java
+++ src/FireEmblem.java
@@ -1,14 +1,21 @@
+import java.net.URL;
+
 public class FireEmblem {
 
     public static void main(String[] args){
-        System.out.println("let's predict what color or type a character will be based on their stats!");
-        train();
-        Character c = new Character();
-        predict(c);
+//        System.out.println("let's predict what color or type a character will be based on their stats!");
+//        train();
+//        Character c = new Character();
+//        predict(c);
+
+        System.out.println("Reading from URL test");
+        String testUrl = "https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet";
+        URL markdown = new URL(testUrl);
+
     }
 
     private static void train(){
-        // import training data:
+        // import training data (all lvl 40 5*):
         // Name,hp,atk,spd,def,res,color,movetype,wpntype,
         // String,int,int,int,int,int,String,String,String,
 
@@ -18,5 +25,16 @@ public class FireEmblem {
 
     }
 
+    /** TODO: MINE STATS FROM SITE, store in SQL database.
+     * Count how many "/ hero / *" entries are
+     * in each letter of
+     * alphabet, store this number in database */
+    private static void mine(){
+        String root = "https://fireemblem.gamepress.gg/hero/";
+        while(){
+
+        }
+
+    }
 
 }
